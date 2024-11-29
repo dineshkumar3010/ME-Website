ef4d41c0bf05e1a3c9459f12e7ee564ace3c71a4
dineshkumar3010 <dineshkumar3010@gmail.com>
2024-11-29 16:04:45 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PropEquityME.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
