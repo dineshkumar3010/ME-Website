9fc1bddd3cced3b167ec7c62e4cf6af22eaf60c6
dineshkumar3010 <dineshkumar3010@gmail.com>
2024-11-29 16:03:28 +0530

    Add files via upload

---
+++ Views/Home/terms_n_conditions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PropEquityME.Views.Home
+{
+    public class terms_n_conditionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
